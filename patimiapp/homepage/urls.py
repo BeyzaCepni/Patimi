6097e9cd9e7a33925987e6baf850e1b531581d41
beyca16 <f.beyza0216@gmail.com>
2024-01-02 00:10:34 +0300

    ilan changed and dynamic links

--- patimiapp/homepage/urls.py
+++ patimiapp/homepage/urls.py
@@ -14,18 +14,18 @@ from. import views
 #http://127.0.0.1:8000/blog => blog
  
 urlpatterns = [
-    path("",views.index),
+    path("",views.index, name="homepage"),
     path("index.html",views.index),
-    path("PatimiBuldun.html",views.patimibuldun),
-    path("PatimiSahiplen.html",views.patimisahiplen),
-    path("PatimiBakilacak.html",views.patimibakilacak),
-    path("PatimiYakinimda.html",views.patimiyakinimda),
-    path("contact.html",views.iletisim),
-    path("about.html",views.hakkimizda),
-    path("blog.html",views.blog),
-    path("loginPage.html",views.loginPage),
-    path("profil.html",views.profil),
-    path("profilEdit.html",views.profilEdit),
-    path("ilan_details.html",views.ilanDetails),
+    path("PatimiBuldun.html",views.patimibuldun, name= "patimibuldun"),
+    path("PatimiSahiplen.html",views.patimisahiplen, name= "patimisahiplen"),
+    path("PatimiBakilacak.html",views.patimibakilacak, name="patimibakilacak"),
+    path("PatimiYakinimda.html",views.patimiyakinimda, name= "patimiyakinimda"),
+    path("contact.html",views.iletisim, name= "iletisim"),
+    path("about.html",views.hakkimizda, name= "hakkimizda"),
+    path("blog.html",views.blog, name="patimikonusalim"),
+    path("loginPage.html",views.loginPage, name= "loginpage"),
+    path("profil.html",views.profil, name= "profil"),
+    path("profilEdit.html",views.profilEdit, name= "profiledit"),
+    path("ilandetail/<int:id>",views.ilanDetails, name= "ilandetails"),
  ]
  
